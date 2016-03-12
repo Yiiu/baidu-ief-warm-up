c375da8fd473c804429b4ce64d224b752bbe41e6
Yu <yu.gintoki@gmail.com>
2016-03-12 18:08:40 +0800

    v0.9

--- src/js/my.js
+++ src/js/my.js
@@ -1,6 +1,30 @@
 (function(){
     var header = document.getElementsByTagName('header')[0];
-    var sheader = setInterval(function(){
-        header.style.opacity = "1";
+    var content = document.getElementsByClassName('content')[0];
+    var profile = document.getElementById('profile');
+    var int_box = document.getElementsByClassName('int-box');
+    var turn = false;
+    var sheader = setInterval(function(){        
+        if(turn){
+            clearInterval(sheader);
+        }else{
+            header.style.opacity = "1";
+            content.className = "content me";
+            turn = true;
+        }
     },1000)
+    var pset = setInterval(function(){
+        if(turn){
+            clearInterval(pset);
+        }else{
+            
+            for(var i=0;i<int_box.length;i++){
+                var c = i*100;
+                var c_i = setInterval(function(){
+                    int_box[i].style.opacity = "1";
+                },c)
+            }
+            turn = true;
+        }
+    },1100)
 })()
